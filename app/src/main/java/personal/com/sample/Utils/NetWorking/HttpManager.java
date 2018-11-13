11f164062dab8cac5c6fcfe5cf24927fa407c757
sidsidq91 <saeedghotb@gmail.com>
2018-11-13 19:37:51 +0330

    added basic authentication

--- app/src/main/java/personal/com/sample/Utils/NetWorking/HttpManager.java
+++ app/src/main/java/personal/com/sample/Utils/NetWorking/HttpManager.java
@@ -1,5 +1,7 @@
 package personal.com.sample.Utils.NetWorking;
 
+import android.util.Base64;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -26,6 +28,48 @@ public class HttpManager {
             e.printStackTrace();
             return null;
         } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            if (reader != null){
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+        }
+    }
+
+    //request with basic authorization
+    public static String getData(String uri, String userName, String passWord){
+        BufferedReader reader = null;
+        HttpURLConnection connection = null;
+        byte[] loginBytes = (userName + ":" + passWord).getBytes();
+        StringBuilder loginbBuilder = new StringBuilder()
+                .append("Basic ")
+                .append(Base64.encodeToString(loginBytes, Base64.DEFAULT));
+        try {
+            URL url = new URL(uri);
+            connection = (HttpURLConnection) url.openConnection();
+
+            connection.addRequestProperty("Authorization", loginbBuilder.toString());
+
+            StringBuilder sb = new StringBuilder();
+            reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+            while ((line = reader.readLine())!=null){
+                sb.append(line + "\n");
+            }
+            return sb.toString();
+        } catch (Exception e) {
+            int status;
+            try {
+                status = connection.getResponseCode();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
             e.printStackTrace();
             return null;
         } finally {
