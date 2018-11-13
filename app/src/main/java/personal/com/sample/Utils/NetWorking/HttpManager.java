81fcdb4f863447712e26895ccd18dafc53b3b164
sidsidq91 <saeedghotb@gmail.com>
2018-11-13 18:55:59 +0330

    added HttpManager Class

---
+++ app/src/main/java/personal/com/sample/Utils/NetWorking/HttpManager.java
@@ -0,0 +1,42 @@
+package personal.com.sample.Utils.NetWorking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class HttpManager {
+
+    public static String getData(String uri){
+        BufferedReader reader = null;
+        try {
+            URL url = new URL(uri);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            StringBuilder sb = new StringBuilder();
+            reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+            while ((line = reader.readLine())!=null){
+                sb.append(line + "\n");
+            }
+            return sb.toString();
+        }catch (MalformedURLException e) {
+            e.printStackTrace();
+            return null;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            if (reader != null){
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+        }
+    }
+}
